060d554fe0942c9a1e37edad61a892934aef36d7
Dimitar Kumanov <dimitark@uw.edu>
2018-01-13 20:29:45 -0800

    Smoothened screen-scrolling
    Remade screen-scrolling to start repeating a screen move at regular
    intervals on key down and stop scrolling on key up or bounds reached.
    
    Scrolling in 2 directions is still a little akward since the screen
    scrolls happen out of sync. This could be fixed by creating additional
    methods for movingUpLeft etc.

--- controller/controller.js
+++ controller/controller.js
@@ -8,39 +8,140 @@ AntColony.Controller = function(params) {
     this.board = params.board;
     this.camera = params.camera;
     this.scale = params.scale;
+
+    this.moveLeftTimeoutId = 0;
+    this.moveRightTimeoutId = 0;
+    this.moveUpTimeoutId = 0;
+    this.moveDownTimeoutId = 0;
+
+    this.isMovingLeft = false;
+    this.isMovingRight = false;
+    this.isMovingUp = false;
+    this.isMovingDown = false;
 };
 
 AntColony.Controller.prototype.start = function(){
     const LEFT = 37, RIGHT = 39, UP = 38, DOWN = 40;
     const that = this;
+    const timeoutDelay = 50;
 
-    window.addEventListener("keydown", function (event) {
+    $(document).keydown(function (event) {
         const keyCode = event.keyCode ? event.keyCode : event.which;
         switch(keyCode){
             case LEFT: //LEFT key
-                if(that.camera.x >= that.scale) { //up key
-                    that.camera.x -= that.scale;
-                    that.board.setAllRegionsChanged();
+                if(!that.isMovingLeft){
+                    that.isMovingLeft = true;
+                    that.moveLeft(timeoutDelay);
                 }
                 break;
             case RIGHT: //RIGHT key
-                if (that.camera.x <= that.camera.maxX - that.scale) {
-                    that.camera.x += that.scale;
-                    that.board.setAllRegionsChanged();
+                if(!that.isMovingRight){
+                    that.isMovingRight = true;
+                    that.moveRight(timeoutDelay);
                 }
                 break;
             case UP: //UP key
-                if(that.camera.y >= that.scale) {
-                    that.camera.y -= that.scale;
-                    that.board.setAllRegionsChanged();
+                if(!that.isMovingUp){
+                    that.isMovingUp = true;
+                    that.moveUp(timeoutDelay);
                 }
                 break;
             case DOWN: //DOWN key
-                if (that.camera.y <= that.camera.maxY - that.scale) {
-                    that.camera.y += that.scale;
-                    that.board.setAllRegionsChanged();
+                if(!that.isMovingDown){
+                    that.isMovingDown = true;
+                    that.moveDown(timeoutDelay);
                 }
                 break;
         }
-    }, false);
+    });
+
+    $(document).keyup(function (event) {
+        const keyCode = event.keyCode ? event.keyCode : event.which;
+        switch(keyCode){
+            case LEFT: //LEFT key
+                that.stopMovingLeft();
+                break;
+            case RIGHT: //RIGHT key
+                that.stopMovingRight();
+                break;
+            case UP: //UP key
+                that.stopMovingUp();
+                break;
+            case DOWN: //DOWN key
+                that.stopMovingDown();
+                break;
+        }
+    });
+};
+
+AntColony.Controller.prototype.moveRight = function(timeoutDelay){
+    const that = this;
+    if (this.camera.x <= this.camera.maxX - this.scale) {
+        this.camera.x += this.scale;
+        this.board.setAllRegionsChanged();
+        this.moveRightTimeoutId = setTimeout(function(){
+            that.moveRight(timeoutDelay);
+        }, timeoutDelay);
+    }else{
+        this.stopMovingRight();
+    }
+};
+
+AntColony.Controller.prototype.moveLeft = function(timeoutDelay){
+    const that = this;
+    if(this.camera.x >= this.scale) {
+        this.camera.x -= this.scale;
+        this.board.setAllRegionsChanged();
+        this.moveLeftTimeoutId = setTimeout(function(){
+            that.moveLeft(timeoutDelay);
+        }, timeoutDelay);
+    }else{
+        this.stopMovingLeft();
+    }
+};
+
+AntColony.Controller.prototype.moveUp = function(timeoutDelay){
+    const that = this;
+    if(this.camera.y >= this.scale) {
+        this.camera.y -= this.scale;
+        this.board.setAllRegionsChanged();
+        this.moveUpTimeoutId = setTimeout(function(){
+            that.moveUp(timeoutDelay);
+        }, timeoutDelay);
+    }else{
+        this.stopMovingUp();
+    }
+};
+
+AntColony.Controller.prototype.moveDown = function(timeoutDelay){
+    const that = this;
+    if (this.camera.y <= this.camera.maxY - this.scale) {
+        this.camera.y += this.scale;
+        this.board.setAllRegionsChanged();
+        this.moveDownTimeoutId = setTimeout(function(){
+            that.moveDown(timeoutDelay);
+        }, timeoutDelay);
+    }else{
+        this.stopMovingDown();
+    }
+};
+
+AntColony.Controller.prototype.stopMovingLeft = function(){
+    clearTimeout(this.moveLeftTimeoutId);
+    this.isMovingLeft = false;
+};
+
+AntColony.Controller.prototype.stopMovingRight = function(){
+    clearTimeout(this.moveRightTimeoutId);
+    this.isMovingRight = false;
+};
+
+AntColony.Controller.prototype.stopMovingUp = function(){
+    clearTimeout(this.moveUpTimeoutId);
+    this.isMovingUp = false;
+};
+
+AntColony.Controller.prototype.stopMovingDown = function(){
+    clearTimeout(this.moveDownTimeoutId);
+    this.isMovingDown = false;
 };
